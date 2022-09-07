13d67ce535722374d3932254723bdccce73eca2b
hoda ahmadzadeh <hoda.ahmadzadeh@gmail.com>
2022-09-07 08:48:49 +0430

    fix some changes

--- hooks/index.js
+++ hooks/index.js
@@ -1,4 +1,3 @@
-export * from "./useRotateGradient";
 export * from "./useTheme";
 export * from "./useBookmrark";
 export * from "./useMobile";
--- hooks/useBookmrark.js
+++ hooks/useBookmrark.js
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+
 import { useColorSet, useColorSetActions } from "../context/RotateProvider";
 import { HOME_CONST } from "../constants";
 
